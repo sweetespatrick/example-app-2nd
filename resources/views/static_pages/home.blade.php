2a07ea79eaefc37e82fdebc8aac8f2477b0e3130
patrick <tianhuai.shen@gmail.com>
2022-04-09 18:20:05 +0800

    修改页面导航栏下边距

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.default')
-@section('title','home')
 @section('content')
-<div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p clss="lead">
-        你现在看到的是<a href="https://www.bing.com">Laravel 入门教程</a>的示范项目主页。
-    </p>
-    <p>
-        一切，从这里开始。
-    </p>
-    <p>
-        <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
-    </p>
-</div>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p clss="lead">
+          你现在看到的是<a href="https://www.bing.com">Laravel 入门教程</a>的示范项目主页。
+        </p>
+        <p>
+          一切，从这里开始。
+        </p>
+        <p>
+          <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
