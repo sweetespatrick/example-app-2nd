016d83938267970c5152b6a7620ebc7bda443fd7
patrick <tianhuai.shen@gmail.com>
2022-04-10 14:43:32 +0800

    切割头部和尾部子试图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="https://www.bing.com" target=_blank>刻意练习</a>
+
+    <div class="float-end">
+      <a href="/about">关于</a>
+    </div>
+</footer>
