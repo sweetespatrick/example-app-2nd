91813823080ac499b146266829b91695b98c4892
patrick <tianhuai.shen@gmail.com>
2022-04-18 14:30:29 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','你已成功退出！');
+        return redirect('login');
+    }
 }
