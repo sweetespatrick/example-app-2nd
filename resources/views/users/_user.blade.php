69afdfac25995b1eefc908fee974e59098bb008f
patrick <tianhuai.shen@gmail.com>
2022-04-22 16:40:19 +0800

    管理员可删除用户

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,4 +1,12 @@
+
 <div class="list-group-item">
     <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
     <a href="{{ route('users.show',$user) }}">{{ $user->name }}</a>
+    @can('destroy',$user)
+        <form action="{{ route('users.destroy',$user->id) }}" method="POST" class="float-end"> <!--原文float-right是错误的,已更新为float-end.-->
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+        </form>
+    @endcan
 </div>
