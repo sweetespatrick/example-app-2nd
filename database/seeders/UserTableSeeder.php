ead6aba1b7f1665ec5271698f15319d5966773a8
patrick <tianhuai.shen@gmail.com>
2022-04-23 15:01:39 +0800

    管理员可删除用分支户部分更改

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -15,6 +15,13 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::factory()->count(50)->create();
+
+        $user = User::find(1);
+        $user->name ='Summer';
+        $user->email = 'tianhuai.shen@gmail.com';
+        $user->is_admin = true;
+        $user->save();
+
     }
     
 }
