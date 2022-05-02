b3126cd61178ff79a6ffb12483a0d9c107a8d03a
patrick <tianhuai.shen@gmail.com>
2022-05-02 16:24:08 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "https://gravatar.loli.net/avatar/$hash?s=$size";
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
